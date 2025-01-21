4075b0d530a682d45cb1aad97b6f17ce2e102ad7
daorozc <daorozc@bancolombia.com.co>
2025-01-21 10:35:26 -0500

    Refactor Jest configuration to use ES modules; add TypeScript configuration for tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-module.exports = {
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
   preset: 'ts-jest',
   rootDir: path.join(__dirname),
   verbose: true,
