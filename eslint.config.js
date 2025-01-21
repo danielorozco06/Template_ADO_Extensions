4d68778c748b3ed52761b138996518bcab48e8de
daorozc <daorozc@bancolombia.com.co>
2025-01-21 14:46:54 -0500

    Refactor ESLint configuration to streamline TypeScript integration and enhance rule enforcement

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,29 @@
 import js from '@eslint/js';
-import typescript from '@typescript-eslint/eslint-plugin';
-import typescriptParser from '@typescript-eslint/parser';
-import prettier from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
 export default [
   js.configs.recommended,
   {
     files: ['**/*.ts'],
     languageOptions: {
-      parser: typescriptParser,
+      parser: tsParser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module'
+        project: './tsconfig.json'
       },
       globals: {
-        console: 'readonly',
-        process: 'readonly'
+        console: 'readonly'
       }
     },
     plugins: {
-      '@typescript-eslint': typescript,
-      'prettier': prettier
+      '@typescript-eslint': tsPlugin
     },
     rules: {
-      ...typescript.configs.recommended.rules,
-      ...prettierConfig.rules,
-      'prettier/prettier': 'error',
-      '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/explicit-function-return-type': 'error',
       '@typescript-eslint/no-explicit-any': 'error',
-      'no-console': 'off'
+      '@typescript-eslint/prefer-readonly': 'error',
+      '@typescript-eslint/no-namespace': 'error',
+      ...tsPlugin.configs.recommended.rules
     }
   }
 ];
